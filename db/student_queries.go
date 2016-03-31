59b266b6d5fb18577d3ffafeb50837b87fad00ff
David Skeppstedt <david.skeppstedt@gmail.com>
2016-03-31 21:57:34 +0200

    fixed a bug.

--- db/student_queries.go
+++ db/student_queries.go
@@ -47,7 +47,7 @@ func (this *Database) ReadCourseStudent(id int) (courses []models.Course) {
 	}
 	for rows.Next() {
 		var tmp models.Course
-		rows.Scan(&tmp.Name,&tmp.Id)
+		rows.Scan(&tmp.Name, &tmp.Id)
 		courses = append(courses, tmp)
 	}
 	return
